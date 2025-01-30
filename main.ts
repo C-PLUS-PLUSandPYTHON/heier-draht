8191f6e13bcbb05528c9c62f5f99f5336ff7af30
C-PLUS-PLUSandPYTHON <141658152+C-PLUS-PLUSandPYTHON@users.noreply.github.com>
2025-01-30 13:28:55 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,58 @@
+function set_animation () {
+    start_animation = [
+    images.createImage(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `),
+    images.createImage(`
+        # # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # #
+        `),
+    images.createImage(`
+        # # # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # # #
+        `),
+    images.createImage(`
+        # # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # #
+        `),
+    images.createImage(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        `),
+    images.createImage(`
+        . # # # #
+        . . . . #
+        . . . . .
+        # . . . .
+        # # # # .
+        `)
+    ]
+}
+function show_animation (anim: any[]) {
+    for (let index = 0; index < anim.length; index++) {
+        anim[0].showImage(0)
+        basic.pause(100)
+    }
+}
+let start_animation: Image[] = []
+set_animation()
+show_animation(start_animation)
 basic.forever(function () {
 	
 })
